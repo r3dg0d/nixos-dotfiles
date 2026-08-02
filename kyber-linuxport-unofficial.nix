54e7a3230f3181646ae7927335a8c435ff48ca6d
r3dg0d <vincentchamberszero@gmail.com>
2026-08-01 18:21:06 -0700

    fix kyber rendering on niri

--- kyber-linuxport-unofficial.nix
+++ kyber-linuxport-unofficial.nix
@@ -30,11 +30,11 @@ let
     inherit pname version src;
 
     extraBwrapArgs = [
-      ''--setenv WAYLAND_DISPLAY "$WAYLAND_DISPLAY"''
+      ''--setenv DISPLAY "$DISPLAY"''
       ''--setenv XDG_RUNTIME_DIR "$XDG_RUNTIME_DIR"''
       ''--setenv DBUS_SESSION_BUS_ADDRESS "$DBUS_SESSION_BUS_ADDRESS"''
-      ''--ro-bind-try "$XDG_RUNTIME_DIR/$WAYLAND_DISPLAY" "$XDG_RUNTIME_DIR/$WAYLAND_DISPLAY"''
       ''--ro-bind-try "$XDG_RUNTIME_DIR/bus" "$XDG_RUNTIME_DIR/bus"''
+      ''--ro-bind-try "/tmp/.X11-unix/X''${DISPLAY#:}" "/tmp/.X11-unix/X''${DISPLAY#:}"''
     ];
 
     extraPkgs = pkgs: with pkgs; [
@@ -71,10 +71,9 @@ symlinkJoin {
     rm $out/bin/kyber-linuxport-unofficial
     makeWrapper ${appimage}/bin/kyber-linuxport-unofficial $out/bin/kyber-linuxport-unofficial \
       --run 'mkdir -p "''${XDG_CONFIG_HOME:-$HOME/.config}/kyber-linuxport"
-             printf "%s\n" wayland > "''${XDG_CONFIG_HOME:-$HOME/.config}/kyber-linuxport/backend"
-             nvidia_icd=/run/opengl-driver/share/glvnd/egl_vendor.d/10_nvidia.json
-             [ -e "$nvidia_icd" ] && export __EGL_VENDOR_LIBRARY_FILENAMES="$nvidia_icd"' \
-      --set GDK_BACKEND wayland \
+             printf "%s\n" x11 > "''${XDG_CONFIG_HOME:-$HOME/.config}/kyber-linuxport/backend"' \
+      --set GDK_BACKEND x11 \
+      --set GTK_IM_MODULE gtk-im-context-simple \
       --set NIXOS_OZONE_WL 1 \
       --set MOZ_ENABLE_WAYLAND 1
 
